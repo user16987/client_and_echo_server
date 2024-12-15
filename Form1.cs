f687f0a09e10bde549568931b04a6b696d827a45
Светлана Клименко < @DESKTOP-2I06CAG>
2024-12-15 13:56:25 +1000

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client_echo_server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NamedPipeClientStream pipe;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pipe = new NamedPipeClientStream("myPipe");
+            pipe.Connect();            
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamWriter wr = new StreamWriter(pipe);
+            StreamReader rd = new StreamReader(pipe);
+            wr.AutoFlush = true;
+            wr.WriteLine(textBox1.Text);
+            textBox1.Text = "";
+            string str = rd.ReadLine();
+            textBox2.Text += str;            
+        }
+    }
+}
